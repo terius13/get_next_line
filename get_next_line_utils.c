4c31cb58ffc585373fa755cfb6763f36a757ecf0
Timberly Ng <ting@c2r5s3.42singapore.sg>
2023-10-10 21:40:26 +0800

    slightly updated, reading buffer whole

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,39 @@
 /*   By: ting <ting@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 16:19:28 by ting              #+#    #+#             */
-/*   Updated: 2023/10/08 16:19:47 by ting             ###   ########.fr       */
+/*   Updated: 2023/10/10 17:48:41 by ting             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+char	*ft_strdup(const char *src)
+{
+	char	*dest;
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (src[i] != '\0')
+		i++;
+	dest = (char *)malloc(sizeof(char) * i + 1);
+	if (dest == NULL)
+		return (NULL);
+	while (src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	char	src[] = "helloworld";
+
+	printf("%s", ft_strdup(src));
+}
+*/
